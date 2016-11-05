6043efa6781c02150d570d5727269cee4fa64cc7
KevinFTXX <chenjin2011@u.northwestern.edu>
2016-11-04 21:03:15 -0400

    Update and rename Two Sum to Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,18 @@
+""" Given an array of integers, return indices of the two numbers such that they add up to a specific target. 
+""" You may assume that each input would have exactly one solution.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+        for index, value in enumerate(nums):
+            hash_map[value] = index
+        for index1, value in enumerate(nums):
+            if target - value in hash_map:
+                index2 = hash_map[target - value]
+                if index1 != index2:
+                    return [index1 + 1, index2 + 1]
