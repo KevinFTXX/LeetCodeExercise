0a5e25ae012e09737fd35646c69e0a190e7ccfca
KevinFTXX <chenjin2011@u.northwestern.edu>
2016-11-04 22:23:37 -0400

    old one is too slow

--- Two Sum.py
+++ Two Sum.py
@@ -13,13 +13,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hash_map = {}   # create a dictionary, each entry in the dictionary is  key: value
-        for index, value in enumerate(nums): # iterate the index and value of all the entry in the list nums, a tuple (index, value) is generated
-            hash_map[value] = index   # the value in nums become the key and it stores the index of that value in nums
-        # the 1st loop go through all the entris in nums and create a dict
-        
+        hash_map = {};
         for index1, value in enumerate(nums):   # now relook at the list nums again
-            if target - value in hash_map: # check if the "target-value" in the key of hash_map
-                index2 = hash_map[target - value]   # if it is true, then get the index of "target-value"
+            complement = target - value
+            if complement not in hash_map:
+                hash_map.update({value:index1})
+            else:
+                index2 = hash_map[complement]   # if it is true, then get the index of "target-value"
                 if index1 != index2:                # check whether these 2 indexs are different, e.g., target = 10, value = 5, index1 = 1, in this case index2 = 1 too, so index1 can equal to index2
-                    return [index1 + 1, index2 + 1] # 
+                    
+                    return [min(index2, index1),max(index1,index2)]
